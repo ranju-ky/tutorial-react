1a40a3a0d5c356a0a9a11ed1ccf8d3e42aefb287
Ranju- <ranjuprj98@gmail.com>
2021-07-13 10:54:27 +0200

    tutoriel-react

--- src/App.js
+++ src/App.js
@@ -1,12 +1,57 @@
-import React from 'react';
+import React, {Component} from 'react';
+import Tabla from './Tabla';
+import Form from './Form';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Tutorial de React de Neoguias!</h1>
-      </div>
-    );
-  }
+class App extends Component {
+    state = {
+        personas: [
+
+            {
+                nombre: 'Clark',
+                apellido: 'Kent',
+            },
+
+            {
+                nombre: 'Bruce',
+                apellido: 'Wayne',
+            },
+            {
+                nombre: 'Peter',
+                apellido: 'Parker',
+            },
+            {
+                nombre: 'Selina',
+                apellido: 'Kyle',
+            },
+        ]
+    }
+    enviarFormulario = (persona) => {
+        this.setState({ personas: [...this.state.personas, persona] });
+
+    }
+
+
+    eliminarPersona = (indice) => {
+        const {personas} = this.state
+
+        this.setState({
+            personas: personas.filter((personas, i) => {
+                return i !== indice;
+            }),
+        });
+    }
+
+    render() {
+        const {personas} = this.state;
+        return (
+            <div className="App">
+                <h1>Tutorial de React</h1>
+                <Tabla datosPersonas={personas} eliminarPersona={this.eliminarPersona}/>
+                <Form enviarFormulario={this.enviarFormulario} />
+            </div>
+        );
+    }
 }
+
 export default App;
+
